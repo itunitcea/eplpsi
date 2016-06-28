77f537c6431d34801a702457c55c9d781fb3cc38
Chandanie Edussuriya <chedus1969@gmail.com>
2016-06-28 13:37:26 +0530

    route andother common

--- cea/config/app.php
+++ cea/config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -91,7 +91,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+   'key' => 'xlhF31NeOlibJcoOW9tvZg7TkHcAZI3n',
 
     'cipher' => 'AES-256-CBC',
 
