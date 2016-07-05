8a67e1e2ed5cb68d3a9538f555c48525d46d518f
leighmedy <tloleighlunas@gmail.com>
2016-07-05 13:26:20 +0530

    July5 Update PUBLIC files

---
+++ cea/public/js/selectIndustry.js
@@ -0,0 +1,63 @@
+$('.specificindustry').on('change', function(){
+if($(this).val() == 'stndrd1_01')
+{	
+$('.specificindustry-link').attr('href', '/envimonitoring/specific/' + $(this).val());	
+}
+})
+
+$('.specificindustry').on('change', function(){
+if($(this).val() == 'stndrd2_02')
+{	
+$('.specificindustry-link').attr('href', '/envimonitoring/specific/' + $(this).val());	
+}
+})
+
+$('.specificindustry').on('change', function(){
+if($(this).val() == 'stndrd3_03')
+{	
+$('.specificindustry-link').attr('href', '/envimonitoring/specific/' + $(this).val());	
+}
+})
+
+$('.specificindustry').on('change', function(){
+if($(this).val() == 'stndrd1_4_04')
+{	
+$('.specificindustry-link').attr('href', '/envimonitoring/specific/' + $(this).val());	
+}
+})
+
+$('.specificindustry').on('change', function(){
+if($(this).val() == 'stndrd1_5_05')
+{	
+$('.specificindustry-link').attr('href', '/envimonitoring/specific/' + $(this).val());	
+}
+})
+
+$('.specificindustry').on('change', function(){
+if($(this).val() == 'stndrd1_6_06')
+{	
+$('.specificindustry-link').attr('href', '/envimonitoring/specific/' + $(this).val());	
+}
+})
+
+$('.specificindustry').on('change', function(){
+if($(this).val() == 'stndrd7_07')
+{	
+$('.specificindustry-link').attr('href', '/envimonitoring/specific/' + $(this).val());	
+}
+})
+
+$('.specificindustry').on('change', function(){
+if($(this).val() == 'stndrd3_6_08')
+{	
+$('.specificindustry-link').attr('href', '/envimonitoring/specific/' + $(this).val());	
+}
+})
+
+$('.specificindustry').on('change', function(){
+if($(this).val() == 'stndrd7_09')
+{	
+$('.specificindustry-link').attr('href', '/envimonitoring/specific/' + $(this).val());	
+}
+})
+
